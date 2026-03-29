6fb7707dc9374d1471b15d52730a9706d0d6fc37
walderneyjuniors-wj <202603546322@alunos.estacio.br>
2026-03-29 21:27:44 +0000

    Nesse código eu criei um menu para o jogador escolher se quer começar o jogo, ler as regras ou sair. Agora o jogador também escolhe qual atributo quer usar para a disputa antes de cadastrar as cartas. Tirei aquelas partes que mostravam '1 e 0' na comparação para o visual ficar mais limpo e usei estruturas de decisão para mostrar qual carta venceu de acordo com o atributo que foi escolhido pelo usuário.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -23,12 +23,56 @@ int main() {
   fgets(jogador, 30, stdin);
   jogador[strcspn(jogador, "\n")] = 0;
 
+  // Menu interativo para o usuario escolher entre iniciar o jogo, ler as regras ou sair do jogo
+  int opcao;
+  int escolhausuario;
+  printf("\n   ---   MENU   ---   \n");
+  printf("Escolha uma opção:\n\n");
+  printf("1 - Iniciar o jogo\n");
+  printf("2 - Regras do jogo\n");
+  printf("3 - Sair\n");
+  scanf(" %d", &opcao);
+
+
+  switch (opcao) {
+  case 1:
+    printf("\n   ---   Para começar o jogo você deverá preencher as informações das suas cartas   ---   \n"); 
+    printf("\nMas antes escolha qual atributo deseja usar para comparar as cartas, e determinar o vencedor.\n\n");
+
+    printf("1 - População\n");
+    printf("2 - Área em km²\n");
+    printf("3 - PIB\n");
+    printf("4 - Número de Pontos Turísticos\n");
+    printf("5 - Densidade Populacional\n");
+    scanf(" %d", &escolhausuario);
+
+    switch (escolhausuario) {
+      case 1:
+        printf("\n   ---   Atributo escolhido: POPULAÇÃO   ---   ");
+        break;
+      case 2:
+        printf("\n   ---   Atributo escolhido: ÁREA EM KM²   ---   ");
+        break;
+      case 3:
+        printf("\n   ---   Atributo escolhido: PIB   ---   ");
+        break;
+      case 4:
+        printf("\n   ---   Atributo escolhido: NÚMERO DE PONTOS TURÍSTICOS   ---   ");
+        break;
+      case 5:
+        printf("\n   ---   Atributo escolhido: DENSIDADE POPULACIONAL   ---   ");
+        break;
+    }
+
+    printf("\n   ---   Ótima escolha! Agora vamos preencher as informações das suas cartas.   ---   \n");
+
   // Instruções ao jogador (usuário).
   printf("\n   ---   Para começar o jogo você deverá preencher as informações das suas cartas   ---   \n"); 
   printf("Por favor, preencha as informações das suas cartas\n");
 
   // Estado da Carta 01
   printf("Qual o Estado da CARTA 01?   ---   ETAPA 1/7\n");
+  fgets(estado01, 30, stdin);
   fgets(estado01, 30, stdin);
   estado01[strcspn(estado01, "\n")] = 0;
 
@@ -105,7 +149,7 @@ int main() {
 
   printf("\n   ---   Pronto! Foram preenchidas todas as informações.   ---   \n");
   
-  printf("\n As suas cartas são:\n");
+  /*printf("\n As suas cartas são:\n");
 
   printf("\n---------------------------------\n");
   printf("            CARTA 01            \n");
@@ -136,33 +180,103 @@ int main() {
   printf("Densidade Populacional: %.2f hab/km²\n", densidade02);
   printf("PIB per Capita: %.2f Reais\n", percapita02);
   printf("Super Poder: %.2f\n", superpoder02);
-  printf("---------------------------------\n");
+  printf("---------------------------------\n");*/
 
 
   printf("\n   ---   BOA SORTE %s!   ---   \n", jogador);
 
-  printf("\nCOMPARAÇÃO ENTRE AS CARTAS:\n");
+  /*printf("\nCOMPARAÇÃO ENTRE AS CARTAS:\n");
   printf("População: %d\n", populacao01 > populacao02);
   printf("Área: %d\n", area01 > area02);
   printf("PIB: %d\n", pib01 > pib02);
   printf("Pontos Turísticos: %d\n", passeio01 > passeio02);
   printf("Densidade Populacional: %d\n", densidade01 < densidade02);
   printf("PIB per Capita: %d\n", percapita01 > percapita02);
-  printf("Super Poder: %d\n", superpoder01 > superpoder02);
+  printf("Super Poder: %d\n", superpoder01 > superpoder02);*/
 
-    // Comparação de cartas
+    // Comparação de cartas e lógica de comparação
   printf("\n   ---   COMPARAÇÃO FINAL ENTRE AS CARTAS   ---   \n");
-  printf("Atributo escolhido: PIB per capita\n");
-  printf("CARTA 01: %s com PIB per capita de %.2f Reais\n", cidade01, percapita01);
-  printf("CARTA 02: %s com PIB per capita de %.2f Reais\n", cidade02, percapita02);
+  printf("\nCidade da CARTA 01: %s   ||   Cidade da CARTA 02: %s\n", cidade01, cidade02);
+  switch (escolhausuario) {
+      case 1:
+        printf("Atributo escolhido: POPULAÇÃO\n");
+        printf("População da CARTA 01: %d habitantes   ||   População da CARTA 02: %d habitantes\n", populacao01, populacao02);
+        if (populacao01 > populacao02) {
+          printf("CARTA 01 é a vencedora com uma população de %d habitantes!\n", populacao01);
+        } else if (populacao02 > populacao01) {
+          printf("CARTA 02 é a vencedora com uma população de %d habitantes!\n", populacao02);
+        } else {
+          printf("Empate! Ambas as cartas têm a mesma população de %d habitantes!\n", populacao01);
+        }
+        break;
+      case 2:
+        printf("Atributo escolhido: ÁREA EM KM²\n");
+        printf("Área da CARTA 01: %.2f Km²   ||   Área da CARTA 02: %.2f Km²\n", area01, area02);
+        if (area01 > area02) {
+          printf("CARTA 01 é a vencedora com uma área de %.2f Km²!\n", area01);
+        } else if (area02 > area01) {
+          printf("CARTA 02 é a vencedora com uma área de %.2f Km²!\n", area02);
+        } else {
+          printf("Empate! Ambas as cartas têm a mesma área de %.2f Km²!\n", area01);
+        }
+        break;
+      case 3:
+        printf("Atributo escolhido: PIB\n");
+        printf("PIB da CARTA 01: %.2f Reais   ||   PIB da CARTA 02: %.2f Reais\n", pib01, pib02);
+        if (pib01 > pib02) {
+          printf("CARTA 01 é a vencedora com um PIB de %.2f Reais!\n", pib01);
+        } else if (pib02 > pib01) {
+          printf("CARTA 02 é a vencedora com um PIB de %.2f Reais!\n", pib02);
+        } else {
+          printf("Empate! Ambas as cartas têm o mesmo PIB de %.2f Reais!\n", pib01);
+        }
+        break;
+      case 4:
+        printf("Atributo escolhido: NÚMERO DE PONTOS TURÍSTICOS\n");
+        printf("Número de Pontos Turísticos da CARTA 01: %d   ||   Número de Pontos Turísticos da CARTA 02: %d\n", passeio01, passeio02);
+        if (passeio01 > passeio02) {
+          printf("CARTA 01 é a vencedora com %d pontos turísticos!\n", passeio01);
+        } else if (passeio02 > passeio01) {
+          printf("CARTA 02 é a vencedora com %d pontos turísticos!\n", passeio02);
+        } else {
+          printf("Empate! Ambas as cartas têm o mesmo número de pontos turísticos: %d!\n", passeio01);
+        }
+        break;
+      case 5:
+        printf("Atributo escolhido: DENSIDADE POPULACIONAL\n");
+        printf("Densidade Populacional da CARTA 01: %.2f hab/km²   ||   Densidade Populacional da CARTA 02: %.2f hab/km²\n", densidade01, densidade02);
+        if (densidade01 < densidade02) {
+          printf("CARTA 01 é a vencedora com uma densidade populacional de %.2f hab/km²!\n", densidade01);
+        } else if (densidade02 < densidade01) {
+          printf("CARTA 02 é a vencedora com uma densidade populacional de %.2f hab/km²!\n", densidade02);
+        } else {
+          printf("Empate! Ambas as cartas têm a mesma densidade populacional de %.2f hab/km²!\n", densidade01);
+        }
+        break;
+    }
+    printf("\n   ---   Fim do jogo.   ---   \n");
 
-  if (percapita01 > percapita02) {
-    printf("\n   ***   A CARTA 01 - %s é a vencedora!   ***   \n", cidade01);
-  } else if (percapita01 < percapita02) {
-    printf("\n   ***   A CARTA 02 - %s é a vencedora!   ***   \n", cidade02);
-  } 
+    break;
+  case 2:
+    printf("\n   ---   REGRAS DO JOGO   ---   \n");
+    printf("1. O jogador deve preencher as informações de duas cartas representando cidades.\n");
+    printf("2. Cada carta deve conter os seguintes atributos: Estado, Código da Carta, Nome da Cidade, População, Área em Km², PIB, Número de Pontos Turísticos, Densidade Populacional, PIB per Capita e Super Poder.\n");
+    printf("3. O jogador deve escolher um atributo para comparar as cartas e determinar o vencedor.\n");
+    printf("4. O vencedor é determinado com base no atributo escolhido (exemplo: maior POPULAÇÃO vence).\n");
+    printf("5. O jogo pode ser jogado novamente preenchendo novas cartas ou escolhendo diferentes atributos para comparação.\n");
+    break;
+
+  case 3:
+  printf("\n   ---   Saindo do jogo...   ---   \n");
+    printf("\n   ---   Obrigado por jogar! Até a próxima!   ---   \n");
+    break;
+
+  default:
+    printf("\n   ---   Opção inválida. Por favor, escolha uma opção válida do menu.   ---   \n");
+    break;
+}
+
 
 
 return 0;
 } 
-
